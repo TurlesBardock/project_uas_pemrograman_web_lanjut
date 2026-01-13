d20be8525906ca989cae013df5cfb768dbc772ef
TurlesBardock <turlesdragonball98@gmail.com>
2026-01-14 05:26:17 +0800

    Fix: only run SQLite exported seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,20 +2,21 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /** 
+    /**
      * Seed the application's database.
      */
     public function run(): void
     {
+        // Hanya jalankan seeder dari data SQLite export
         $this->call([
-            CategorySeeder::class,
-            AdminSeeder::class,
+            UsersTableSeeder::class,
+            NewsCategoriesTableSeeder::class,
+            PostsTableSeeder::class,
+            CategoryPostTableSeeder::class,
         ]);
     }
 }
